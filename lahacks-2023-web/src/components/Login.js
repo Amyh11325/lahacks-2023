fec649d5a14c8afacc42ef8d8f33bbe4f782a889
lzhou0714 <lzhou0714@g.ucla.edu>
2023-04-22 20:56:35 -0700

    login page

--- lahacks-2023-web/src/components/Login.js
+++ lahacks-2023-web/src/components/Login.js
@@ -1,33 +1,63 @@
 import React from "react";
 import { useState } from "react";
+import loginService from '../services/login'
+import { ReactNotifications, Store } from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css';
+
 
 const Login = () => {
-    const [username, setUsername] = useState('') 
+    const [email, setEmail] = useState('') 
     const [password, setPassword] = useState('') 
+    const [user, setUser] = useState(null)
+    const [errorMessage, setErrorMessage] = useState(null)
 
-    const handleLogin = (event) => {
+
+    const handleLogin = async (event) => {
         event.preventDefault()
-        console.log('logging in with', username, password)
+        try {
+            const user = await loginService.login({
+              email, password,
+            })
+            setUser(email)
+            setEmail('')
+            setPassword('')
+            console.log("success")
+          } catch (exception) {
+            console.log(exception)
+            Store.addNotification({
+                title: "",
+                message: 'Wrong credentials',
+                type: "warning",
+                insert: "top",
+                container: "top-center",
+            })
+            console.log("wrong credentials")
+            setTimeout(() => {
+                Store.removeAllNotifications()
+            //   setErrorMessage(null)
+            }, 5000)
+          }
     }
     return (
         <div>
+            <ReactNotifications />
             <h1>Login</h1>
             <form onSubmit={handleLogin}>
             {/* <form> */}
             <div>
-            username
+            email
                 <input
                 type="text"
-                // value={username}
+                value={email}
                 name="Username"
-                onChange={({ target }) => setUsername(target.value)}
+                onChange={({ target }) => setEmail(target.value)}
             />
             </div>
             <div>
             password
                 <input
                 type="password"
-                // value={password}
+                value={password}
                 name="Password"
                 onChange={({ target }) => setPassword(target.value)}
             />
@@ -37,7 +67,6 @@ const Login = () => {
 
         </div>
         
-
     );
 
 }
